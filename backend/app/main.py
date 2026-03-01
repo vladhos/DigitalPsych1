8b37c3f75052c6c11291608a16187b0f263d01fa
vladhos <vladhos@gmail.com>
2026-03-01 20:31:35 +0100

    Add files via upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,12 +11,15 @@ app = FastAPI(
 )
 
 # CORS
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://digitalpsych-frontend.onrender.com",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
